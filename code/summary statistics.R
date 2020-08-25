f201483441d4810157fd0ce19f49eb067a2be6c4
eric <eric.asare@usask.ca>
2020-08-24 23:54:10 -0600

    updated summary stats

--- code/summary statistics.R
+++ code/summary statistics.R
@@ -1,15 +1,33 @@
 
-library("tidyverse")
+rm(list=ls(all=TRUE))
 
-#.......... Summary statistics for wetland meta-data ________
-#..Whole data
+if (!require(pacman)) {
+	install.packages("pacman")
+	library(pacman)
+}
+
+# Load Packages
+p_load(tidyverse, dplyr, summarytools)
+
+# Import data
+#-----------------------------------------------
 meta_data <- read.csv("data/finalData_24_8.csv")
 
-summary_whole <-data.frame(psych::describe(meta_data))
-data2 %>% View()
+#Summary Stats for whole data
+sum_whole <- meta_data %>% descr(stats = "fivenum") %>% tb()
 
-#Summary by category
+# Grouped summary statis
+grouped_wlfresh <- meta_data %>% group_by(wlfresh) %>% descr(stats = "fivenum") %>% tb()
+grouped_canada <- meta_data %>% group_by(canada) %>% descr(stats = "fivenum") %>% tb()
 
-sumary_wlfresh <- data.frame(psych::describeBy(meta_data, meta_data$wlfresh))
-sumary_canada <- data.frame(psych::describeBy(meta_data, meta_data$canada))
+#saving data
+write_csv(sum_whole, "data/sum_whole.csv")
+write_csv(grouped_wlfresh , "data/grouped_wlfresh .csv")
+write_csv(grouped_canada, "data/grouped_canada.csv")
+
+
+
+
+
+
 
